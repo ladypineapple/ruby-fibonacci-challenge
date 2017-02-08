e1cf9fe38c49f6f1818bde250ccdde395576b9e5
ladypinetree <maureenpine@gmail.com>
2017-02-08 09:31:44 -0500

    First hack at FibonacciRange

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,4 +1,25 @@
+# frozen_string_literal: true
+
+#
+
 class FibonacciRange
+  include Enumerable
+
+  def each
+    current_fib = start
+    while current_fib <= stop
+      yield current_fib
+      current_fib + previous_fib = next_fib
+    end
+  end
+
+  attr_reader :start, :stop, :step
+  private :start, :stop, :step
+  def initialize(start, stop, step)
+    @start = start
+    @stop = stop
+    @step = step
+  end
 end
 
 class FibonacciNumber
